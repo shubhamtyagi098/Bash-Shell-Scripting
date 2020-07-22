9b12c5e04f95682c73478c69afdd1aed8b0745a1
shubhamtyagi098 <shubhamtyagiop@gmail.com>
2020-07-22 17:50:36 +0530

    Intial commit

---
+++ basic/getting_arguments.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Author: Shubham Tyagi
+#Description: This file illustrate how to get argument from a user
+
+#totalarg=$#
+ar1=10
+arg2=20
+((result=arg1*arg2))
+#echo "Total args is $totalarg"
+echo $result
